d2540c8be306c1590823c3cb84c21e472af95ea1
will.conklin@dbtlabs.com <will.conklin@dbtlabs.com>
2024-10-24 16:43:50 +0000

    renamed models and moved them to different folders based on best practices. added dbt codegen package

--- models/jaffle_shop/jaffle_shop_customers.sql
+++ models/jaffle_shop/jaffle_shop_customers.sql
@@ -2,13 +2,13 @@ with customers as (
 
     select
         *
-        from {{ref("stg_jaffle_shop_customers")}}
+        from {{ref("stg_jaffle_shop__customers")}}
 
 ),
 
 orders as (
 
-    select * from {{ref("stg_jaffle_shop_orders")}}
+    select * from {{ref("stg_jaffle_shop__orders")}}
 
 ),
 
